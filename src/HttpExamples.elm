7d6e0b75ed598deba8ce15f13cd2bf5615250f18
karlaevelize <k.evelize@gmail.com>
2022-11-04 11:47:13 +0100

    Http request and error handling

--- src/HttpExamples.elm
+++ src/HttpExamples.elm
@@ -5,16 +5,45 @@ import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
 
-type alias Model =
-    List String
+type alias Model = 
+    { nicknames : List String
+    , errorMessage : Maybe String
+    }
 
 view : Model -> Html Msg
 view model =
     div []
-    [ button [ onClick SendHttpRequest] [ text "Get data from server" ]
-    , h3 [] [ text "Old School Main Characters" ]
-    , ul [] [List.map viewNickname model]
-    ]
+        [ button [ onClick SendHttpRequest ]
+            [ text "Get data from server" ]
+        , viewNicknamesOrError model
+        ]
+
+viewNicknamesOrError : Model -> Html Msg
+viewNicknamesOrError model =
+    case model.errorMessage of
+        Just message ->
+            viewError message
+
+        Nothing ->
+            viewNicknames model.nicknames
+
+viewError : String -> Html Msg
+viewError errorMessage =
+    let
+        errorHeading =
+            "Couldn't fetch nicknames at this time."
+    in
+        div []
+            [ h3 [] [ text errorHeading ]
+            , text ("Error: " ++ errorMessage)
+            ]
+
+viewNicknames : List String -> Html Msg
+viewNicknames nicknames =
+    div []
+        [ h3 [] [ text "Old School Main Characters" ]
+        , ul [] (List.map viewNickname nicknames)
+        ]
 
 viewNickname : String -> Html Msg
 viewNickname nickname = 
@@ -46,15 +75,45 @@ update msg model =
                 nicknames =
                     String.split "," nicknamesStr
             in
-            ( nicknames, Cmd.none )
+            ( { model | nicknames = nicknames }, Cmd.none )
 
-        DataReceived (Err _) ->
-            ( model, Cmd.none )
+        DataReceived (Err httpError) ->
+            ( { model
+                | errorMessage = Just (buildErrorMessage httpError)
+              }
+            , Cmd.none
+            )
+
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+init : () -> ( Model, Cmd Msg)
+init _ =
+    ( { nicknames = []
+      , errorMessage = Nothing
+      }
+    , Cmd.none
+    )
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> ( [], Cmd.none )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
